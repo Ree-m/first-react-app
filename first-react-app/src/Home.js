dcf84af31b8e1df51d022b6c6e10def82b37b9b9
Reem <reembsrat@gmail.com>
2022-11-01 23:39:54 +0300

    props

--- first-react-app/src/Home.js
+++ first-react-app/src/Home.js
@@ -1,19 +1,24 @@
-import {useState} from "react";
+import { useState } from "react";
+import BlogList from "./BlogList";
 
 const Home = () => {
-    const [name,setName]=useState("luffy");
+    const [blogs,setBlogs] = useState([
+        { title: 'My new website', body: 'lorem ipsum...', author: 'mario', id: 1 },
+        { title: 'Welcome party!', body: 'lorem ipsum...', author: 'yoshi', id: 2 },
+        { title: 'Web dev top tips', body: 'lorem ipsum...', author: 'mario', id: 3 }
+    ]);
 
-    const handleClick=()=>{
-        setName("staw hat")
 
-    }
-    return (  
+    return (
         <div className="home">
-            <h1>Homepage</h1>
-            <p>{name}</p>
-            <button onClick={handleClick}>Click Me</button>
+          <BlogList blogs={blogs} title={"All Blogs!"}/>
+          <BlogList blogs={blogs.filter( (blog) => blog.author =="mario" )} title={"Mario's Blogs!"}/>
+
         </div>
-    );
-}
- 
+      );
+    }
+     
+
 export default Home;
+
+//name is luffy,but then when i click on the button,handleClick fires,which sets name to straw hat
