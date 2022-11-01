0857ffe66715a4fba1fb896b4294841f589c9b48
Reem <reembsrat@gmail.com>
2022-11-01 03:34:27 +0300

    set up first state

--- first-react-app/src/Home.js
+++ first-react-app/src/Home.js
@@ -1,7 +1,17 @@
+import {useState} from "react";
+
 const Home = () => {
+    const [name,setName]=useState("luffy");
+
+    const handleClick=()=>{
+        setName("staw hat")
+
+    }
     return (  
         <div className="home">
             <h1>Homepage</h1>
+            <p>{name}</p>
+            <button onClick={handleClick}>Click Me</button>
         </div>
     );
 }
--- first-react-app/src/Navbar.js
+++ first-react-app/src/Navbar.js
@@ -1,4 +1,11 @@
 const Navbar = () => {
+    const handleClick= (e)=>{ // e is an event object,has alot of info
+        console.log('Hello',e)
+
+    }
+    const handleClickAgain=(name,e)=>{
+        console.log('hello,httyhgtr' + name ,e)
+    }
     return (  
         <nav className="navbar">
             <h1>The Dojo Blog</h1>
@@ -6,6 +13,11 @@ const Navbar = () => {
             <div className="links">
                 <a href="/">Home</a>
                 <a href="/Create">New Blog</a>
+                <button onClick={handleClick}>Click Me</button>
+                <button onClick={(e)=>{  // an anonymous
+                    handleClickAgain('yoshi',e)
+                }}>Click Me Again</button>
+
             </div>
             
         </nav>
