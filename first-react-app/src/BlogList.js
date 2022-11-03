1cdb4b6fa1210d70df8a62190bf915a7346438ba
Reem <reembsrat@gmail.com>
2022-11-03 13:13:03 +0300

    json server,useEffect dependency,loading  working

--- first-react-app/src/BlogList.js
+++ first-react-app/src/BlogList.js
@@ -1,4 +1,4 @@
-const BlogList = ({blogs,title,handleDelete}) => {
+const BlogList = ({blogs,title}) => {
     return ( 
         <div className="blog-list">
             <h2>{title}</h2>
@@ -6,7 +6,6 @@ const BlogList = ({blogs,title,handleDelete}) => {
             <div className="blog-preview" key={blog.id} >
               <h2>{ blog.title }</h2>
               <p>Written by { blog.author }</p>
-              <button onClick={()=>handleDelete(blog.id)}>Delete Blog</button> 
               {/* after onclick,we have an anonymous function that uses the fuction handleDelte amd passes in an argument */}
             </div>
           ))}
--- first-react-app/src/Home.js
+++ first-react-app/src/Home.js
@@ -1,28 +1,49 @@
-import { useState } from "react";
+
+import { useState, useEffect } from "react"; //these are hooks
 import BlogList from "./BlogList";
 
 const Home = () => {
-    const [blogs,setBlogs] = useState([
-        { title: 'My new website', body: 'lorem ipsum...', author: 'mario', id: 1 },
-        { title: 'Welcome party!', body: 'lorem ipsum...', author: 'yoshi', id: 2 },
-        { title: 'Web dev top tips', body: 'lorem ipsum...', author: 'mario', id: 3 }
-    ]);
-    const handleDelete=(id)=>{
-       const newBlogs= blogs.filter(blog=> blog.id!==id)
-       setBlogs(newBlogs)
+    const [blogs, setBlogs] = useState(null); // null when it first renders,then value changes with useState
+    const [isPending, setIsPending] = useState(true)
 
-    }
+    // every time there is a re-render,thefunction in useEffect runs.
+    // useEffect can also have a second argument(in addition to the fuction),an empty array or a value. The value is called useEffect dependency.
+    // the useEffect function only runs after the value(dependency) cheanges/re renders
+    // here,[name] is a dependency
+    useEffect(() => {
+        setTimeout(() => {
+            fetch("http://localhost:8000/blogs")
+            .then(res => {
 
+                return res.json()
+            })
+            .then(data => {
+                setBlogs(data)
+                setIsPending(false)// when data is available,isPending(false),so loading doesnt show
+            })
+            .catch(err => console.log(err))
+        }, 1000);
 
+    }, [])
+
+            
+        
     return (
         <div className="home">
-          <BlogList blogs={blogs} title={"All Blogs!"} handleDelete={handleDelete} />
+
+            {/* if isPending is true, lodaing appears in browser*/}
+            {isPending && <div>Loading...</div>}
+
+            {/* the && is for: 
+if blogs is null,it wont check the next condition,so nothing return
+ if blogs is not null,the following condition runs(ie,bloglist) */}
+            {blogs && <BlogList blogs={blogs} title={"All Blogs!"} />}
+
 
         </div>
-      );
-    }
-     
+    );
+}
+
 
 export default Home;
 
-//name is luffy,but then when i click on the button,handleClick fires,which sets name to straw hat
